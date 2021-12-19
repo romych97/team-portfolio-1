fad9358762d1f80aeec47e0165d2020772eff7f2
Roman <abram.room@bk.ru>
2021-12-19 05:26:16 +0300

    add top menu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,12 @@
 import '../styles/main.scss'
 
 import MainPage from '../components/MainPage/MainPage'
+import TopMenu from '../components/TopMenu/TopMenu'
 
 function IndexPage() {
     return (
       <div className="dark dark:bg-black dark-mode">
-        <p className="bg-blue-500 dark:bg-red-500">Settings</p>
+        <TopMenu />
         <main className="py-6 px-4 sm:p-6 md:py-10 md:px-8">
           <div  className="max-w-4xl mx-auto lg:max-w-5xl lg:gap-x-20 lg:grid-cols-2">
             <MainPage />
