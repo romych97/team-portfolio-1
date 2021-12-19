8f0628cdd970b93158c6e35f6736966b0f48964e
Roman <abram.room@bk.ru>
2021-12-20 02:44:49 +0300

    design update, add fonts, create background code imitation

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,6 +1,17 @@
 function MainPage() {
     return (
         <div>
+
+            {/* Code imitation background */}
+            <div className="absolute top-36  text-gray-800  select-none cursor-default whitespace-nowrap">
+                <br />button type="button" className="bg-gray-800 p-1"&gt;
+                <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;span className="sr-only"&gt;View notifications&lt;/span&gt;
+                <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"&gt;
+                <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" /&gt;
+                <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;/svg&gt;
+                <br />&lt;/button&gt;
+            </div>
+
             <div>app</div>
             <div>app</div>
             <div>app</div>
